0390b33d9384cdbbd01fd0ae39b2f672c1c631f5
Lam Truong <thailamtruong05@gmail.com>
2022-05-10 10:11:02 +0700

    update product class

--- classes/product.php
+++ classes/product.php
@@ -15,7 +15,7 @@ class product
     }
     public function getAll()
     {
-        $sql = "SELECT * FROM `tbl_sanpham`";
+        $sql = "SELECT * FROM `tbl_sanpham WHERE TrangThaiSanPham = 1";
         $result = $this->db->select($sql);
         if ($result->num_rows > 0) {
             return $result;
@@ -51,7 +51,7 @@ class product
         }
     }
     public function get_count_filter($category,$price_from,$price_to){
-        $sql = "SELECT * FROM `tbl_sanpham` WHERE LoaiSanPham = $category AND GiaSanPham between $price_from and $price_to ";
+        $sql = "SELECT * FROM `tbl_sanpham` WHERE LoaiSanPham = $category AND GiaSanPham between $price_from and $price_to AND TrangThaiSanPham = 1";
         $result = $this->db->select($sql);
         // echo($sql);
         if ($result && $result->num_rows > 0) {
@@ -62,13 +62,13 @@ class product
     }
     public function sort_products_by_category($category,$start,$limit,$sort){
         if ($sort == 'price_asc') { //Tang dan
-            $sql = "SELECT * FROM `tbl_sanpham` where LoaiSanPham = $category ORDER by GiaSanPham ASC limit $start,$limit";
+            $sql = "SELECT * FROM `tbl_sanpham` where LoaiSanPham = $category AND TrangThaiSanPham = 1 ORDER by GiaSanPham ASC  limit $start,$limit";
         } else if ($sort == 'price_desc') { //Giam
-            $sql = "SELECT * FROM `tbl_sanpham` where LoaiSanPham = $category ORDER by GiaSanPham DESC limit $start,$limit";
+            $sql = "SELECT * FROM `tbl_sanpham` where LoaiSanPham = $category AND TrangThaiSanPham = 1 ORDER by GiaSanPham DESC limit $start,$limit";
         } else if ($sort == 'date_asc') {
-            $sql = "SELECT * FROM `tbl_sanpham` where LoaiSanPham = $category ORDER by created_at ASC limit $start,$limit";
+            $sql = "SELECT * FROM `tbl_sanpham` where LoaiSanPham = $category AND TrangThaiSanPham = 1 ORDER by created_at ASC limit $start,$limit";
         } else if ($sort == 'date_desc') {
-            $sql = "SELECT * FROM `tbl_sanpham` where LoaiSanPham = $category ORDER by created_at DESC limit $start,$limit";
+            $sql = "SELECT * FROM `tbl_sanpham` where LoaiSanPham = $category AND TrangThaiSanPham = 1 ORDER by created_at DESC limit $start,$limit";
         }
         $result = $this->db->select($sql);
         if ($result && $result->num_rows > 0) {
@@ -79,13 +79,13 @@ class product
     }
     public function filter_product_by_price_and_sort($category, $start, $limit, $price_from, $price_to,$sort) {
         if ($sort == 'price_asc') { //Tang dan
-            $sql = "SELECT * FROM `tbl_sanpham` where LoaiSanPham = $category AND GiaSanPham between $price_from and $price_to  ORDER by GiaSanPham ASC limit $start,$limit";
+            $sql = "SELECT * FROM `tbl_sanpham` where LoaiSanPham = $category AND TrangThaiSanPham = 1 AND GiaSanPham between $price_from and $price_to  ORDER by GiaSanPham ASC limit $start,$limit";
         } else if ($sort == 'price_desc') { //Giam
-            $sql = "SELECT * FROM `tbl_sanpham` where LoaiSanPham = $category AND GiaSanPham between $price_from and $price_to ORDER by GiaSanPham DESC limit $start,$limit";
+            $sql = "SELECT * FROM `tbl_sanpham` where LoaiSanPham = $category AND TrangThaiSanPham = 1 AND GiaSanPham between $price_from and $price_to ORDER by GiaSanPham DESC limit $start,$limit";
         } else if ($sort == 'date_asc') {
-            $sql = "SELECT * FROM `tbl_sanpham` where LoaiSanPham = $category AND GiaSanPham between $price_from and $price_to ORDER by created_at ASC limit $start,$limit";
+            $sql = "SELECT * FROM `tbl_sanpham` where LoaiSanPham = $category AND TrangThaiSanPham = 1 AND GiaSanPham between $price_from and $price_to ORDER by created_at ASC limit $start,$limit";
         } else if ($sort == 'date_desc') {
-            $sql = "SELECT * FROM `tbl_sanpham` where LoaiSanPham = $category AND GiaSanPham between $price_from and $price_to ORDER by created_at DESC limit $start,$limit";
+            $sql = "SELECT * FROM `tbl_sanpham` where LoaiSanPham = $category AND TrangThaiSanPham = 1 AND GiaSanPham between $price_from and $price_to ORDER by created_at DESC limit $start,$limit";
         }
         $result = $this->db->select($sql);
         if ($result && $result->num_rows > 0) {
@@ -95,7 +95,7 @@ class product
         }
     }
     public function get_products_by_search($key){
-        $sql = "SELECT * FROM `tbl_sanpham` where TenSanPham LIKE '%$key%'";
+        $sql = "SELECT * FROM `tbl_sanpham` where TenSanPham LIKE '%$key%' AND TrangThaiSanPham = 1";
         $result = $this->db->select($sql);
         if ($result && $result->num_rows > 0) {
             return $result;
@@ -104,7 +104,7 @@ class product
         }
     }
     public function get_products_by_search_pagination($key,$start,$limit){
-        $sql = "SELECT * FROM `tbl_sanpham` where TenSanPham LIKE '%$key%' limit $start,$limit";
+        $sql = "SELECT * FROM `tbl_sanpham` where TenSanPham LIKE '%$key%' AND TrangThaiSanPham = 1 limit $start,$limit";
         $result = $this->db->select($sql);
         if ($result && $result->num_rows > 0) {
             return $result;
@@ -119,31 +119,31 @@ class product
             if (($_POST['dataPost'] == 'selling_products')) {
                 //Tạo view
                 // create view sanphambanchay as SELECT count(maSanPham) as msp,maSanPham FROM `tbl_chitietdonhang` GROUP by (maSanPham) ORDER by msp DESC limit 15
-                $sql = "SELECT * from tbl_sanpham WHERE MaSanPham IN (SELECT sanphambanchay.MaSanPham FROM sanphambanchay)";
+                $sql = "SELECT * from tbl_sanpham WHERE MaSanPham IN (SELECT sanphambanchay.MaSanPham FROM sanphambanchay) AND TrangThaiSanPham = 1";
             } else
                     if ($_POST['dataPost'] == 'popular_products') {
-                $sql = "SELECT * FROM `tbl_sanpham` where LoaiSanPham IN(1,2,3) ORDER by RAND()  DESC LIMIT 15";
+                $sql = "SELECT * FROM `tbl_sanpham` where LoaiSanPham IN(1,2,3) AND TrangThaiSanPham = 1 ORDER by RAND()  DESC LIMIT 15 ";
             }
         }
         return $this->db->select($sql) ? $this->db->select($sql) : false;
     }
     public function get_products_by_id($id){
-        $sql = "SELECT * FROM tbl_sanpham,tbl_loaisanpham where MaLoai=LoaiSanPham AND MaSanPham =".$id;
+        $sql = "SELECT * FROM tbl_sanpham,tbl_loaisanpham where MaLoai=LoaiSanPham AND TrangThaiSanPham = 1 AND MaSanPham =".$id;
         return $this->db->select($sql) ? $this->db->select($sql) : false;
     }
     public function getProductByCategory($category){
-        $sql = "SELECT * FROM tbl_sanpham where LoaiSanPham= $category";
+        $sql = "SELECT * FROM tbl_sanpham where LoaiSanPham= $category AND TrangThaiSanPham = 1";
         return $this->db->select($sql) ? $this->db->select($sql) : false;
     }
     public function sort_products_by_search($key,$start,$limit,$sort){
         if ($sort == 'price_asc') { //Tang dan
-            $sql = "SELECT * FROM `tbl_sanpham` where TenSanPham LIKE '%$key%' ORDER by GiaSanPham ASC limit $start,$limit";
+            $sql = "SELECT * FROM `tbl_sanpham` where TenSanPham LIKE '%$key%' AND TrangThaiSanPham = 1 ORDER by GiaSanPham ASC limit $start,$limit";
         } else if ($sort == 'price_desc') { //Giam
-            $sql = "SELECT * FROM `tbl_sanpham` where TenSanPham LIKE '%$key%' ORDER by GiaSanPham DESC limit $start,$limit";
+            $sql = "SELECT * FROM `tbl_sanpham` where TenSanPham LIKE '%$key%' AND TrangThaiSanPham = 1 ORDER by GiaSanPham DESC limit $start,$limit";
         } else if ($sort == 'date_asc') {
-            $sql = "SELECT * FROM `tbl_sanpham` where TenSanPham LIKE '%$key%' ORDER by created_at ASC limit $start,$limit";
+            $sql = "SELECT * FROM `tbl_sanpham` where TenSanPham LIKE '%$key%' AND TrangThaiSanPham = 1 ORDER by created_at ASC limit $start,$limit";
         } else if ($sort == 'date_desc') {
-            $sql = "SELECT * FROM `tbl_sanpham` where TenSanPham LIKE '%$key%' ORDER by created_at DESC limit $start,$limit";
+            $sql = "SELECT * FROM `tbl_sanpham` where TenSanPham LIKE '%$key%' AND TrangThaiSanPham = 1 ORDER by created_at DESC limit $start,$limit";
         }
         $result = $this->db->select($sql);
         if ($result && $result->num_rows > 0) {
@@ -153,7 +153,7 @@ class product
         }
     }
     public function filter_product_by_search($key,$form,$to,$start,$limit){
-        $sql = "SELECT * FROM `tbl_sanpham` where TenSanPham LIKE '%$key%' AND GiaSanPham between $form and $to limit $start,$limit";
+        $sql = "SELECT * FROM `tbl_sanpham` where TenSanPham LIKE '%$key%' AND TrangThaiSanPham = 1 AND GiaSanPham between $form and $to limit $start,$limit";
         $result = $this->db->select($sql);
         if ($result && $result->num_rows > 0) {
             return $result;
@@ -162,7 +162,7 @@ class product
         }
     }
     public function filter_product_by_search_count($key,$form,$to){
-        $sql = "SELECT * FROM `tbl_sanpham` where TenSanPham LIKE '%$key%' AND GiaSanPham between $form and $to ";
+        $sql = "SELECT * FROM `tbl_sanpham` where TenSanPham LIKE '%$key%' AND TrangThaiSanPham = 1 AND GiaSanPham between $form and $to ";
         $result = $this->db->select($sql);
         if ($result && $result->num_rows > 0) {
             return $result;
@@ -183,7 +183,7 @@ class product
     }
 
     public function search_product_by_categories_pagination($key,$category,$start,$limit){
-        $sql = "SELECT * FROM `tbl_sanpham` where TenSanPham LIKE '%$key%' AND LoaiSanPham IN ($category) limit $start,$limit";
+        $sql = "SELECT * FROM `tbl_sanpham` where TenSanPham LIKE '%$key%' AND TrangThaiSanPham = 1 AND LoaiSanPham IN ($category) limit $start,$limit";
         $result = $this->db->select($sql);
         if ($result && $result->num_rows > 0) {
             return $result;
@@ -192,7 +192,7 @@ class product
         }
     }
     public function search_product_by_categories_count($key,$category){
-        $sql = "SELECT * FROM `tbl_sanpham` where TenSanPham LIKE '%$key%' AND LoaiSanPham IN ($category)";
+        $sql = "SELECT * FROM `tbl_sanpham` where TenSanPham LIKE '%$key%' AND TrangThaiSanPham = 1 AND LoaiSanPham IN ($category)";
         $result = $this->db->select($sql);
         if ($result && $result->num_rows > 0) {
             return $result;
@@ -201,7 +201,7 @@ class product
         }
     }
     public function filter_product_by_search_categories_count($key,$form,$to,$category){
-        $sql = "SELECT * FROM `tbl_sanpham` where TenSanPham LIKE '%$key%' AND LoaiSanPham IN ($category) AND GiaSanPham between $form and $to";
+        $sql = "SELECT * FROM `tbl_sanpham` where TenSanPham LIKE '%$key%' AND TrangThaiSanPham = 1 AND LoaiSanPham IN ($category) AND GiaSanPham between $form and $to";
         $result = $this->db->select($sql);
         if ($result && $result->num_rows > 0) {
             return $result;
@@ -210,7 +210,7 @@ class product
         }
     }
     public function filter_product_by_search_categories_pagination($key,$form,$to,$category,$start,$limit){
-        $sql = "SELECT * FROM `tbl_sanpham` where TenSanPham LIKE '%$key%' AND LoaiSanPham IN ($category) AND GiaSanPham between $form and $to limit $start,$limit";
+        $sql = "SELECT * FROM `tbl_sanpham` where TenSanPham LIKE '%$key%' AND TrangThaiSanPham = 1 AND LoaiSanPham IN ($category) AND GiaSanPham between $form and $to limit $start,$limit";
         $result = $this->db->select($sql);
         if ($result && $result->num_rows > 0) {
             return $result;
